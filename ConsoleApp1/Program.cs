d09246c62219bb90fadc282a2aeb23a5afa00194
NgocViet1 <viettruongngoc@gmail.com>
2025-05-17 13:53:25 +0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("viettrai");
+                        Console.WriteLine("viettrai3");
+
         }
     }
 }
